c051b80ad9492d609a7c2e2adfea8d6cb2015e0a
MatsLanGoH <mats.lan.pod@googlemail.com>
2016-11-16 17:27:50 +0900

    Basic logic for DOM manipulation

--- js/main.js
+++ js/main.js
@@ -1,13 +1,52 @@
-/*
- * Event listeners to show/hide inactive channels.
- */
+/**************************************************
+ * API Call to populate channel list
+ **************************************************/
 
+
+
+
+ /**************************************************
+  * Output function to add channel info to DOM
+  **************************************************/
+// TODO Remove auto-executing brackets
+(function addChannelToDom() {
+  // Create outer div first
+  var outerEl = buildElement('div', 'channel-list-item online');
+
+  // Append child elements to outer div
+  for (var i = 0; i < 3; i++) {
+    var items = ['icon', 'name', 'content'];
+    var innerEl = buildElement('div', 'channel-'+items[i], 'Now printing');
+    outerEl.appendChild(innerEl);
+  }
+
+  // Finally append outer div to DOM
+  document.getElementById('channel-list-viewer').appendChild(outerEl);
+}
+)();
+
+
+// TODO: WIP.
+function buildElement(tag, className='', text='') {
+  var el = document.createElement(tag);
+  el.setAttribute('class', className);
+  if (text != '') {
+    el.appendChild(document.createTextNode(text));
+  }
+  return el;
+}
+
+
+/**************************************************
+ * Event listeners to show/hide inactive channels.
+ **************************************************/
 // Set up variables
 var showAll     = document.getElementById('show-all'),
     showOnline  = document.getElementById('show-online'),
     showOffline  = document.getElementById('show-offline');
 
-// Add EventListeners
+
+// Shows all channels (both offline and online)
 showAll.addEventListener('click', function() {
     var elems = document.getElementsByClassName('channel-list-item');
     for (var i = 0; i < elems.length; i++) {
@@ -17,11 +56,12 @@ showAll.addEventListener('click', function() {
     }
 });
 
+
+// Hides offline channels and shows online channels
 showOnline.addEventListener('click', function() {
   var offElems = document.getElementsByClassName('offline'),
       onElems  = document.getElementsByClassName('online');
   for (var i = 0; i < offElems.length; i++) {
-    console.log(offElems[i].style.display);
     offElems[i].style.display = 'none';
   };
   for (var i = 0; i < onElems.length; i++) {
@@ -29,11 +69,12 @@ showOnline.addEventListener('click', function() {
   }
 });
 
+
+// Hides online channels and shows offline channels
 showOffline.addEventListener('click', function() {
   var offElems = document.getElementsByClassName('offline'),
       onElems  = document.getElementsByClassName('online');
   for (var i = 0; i < offElems.length; i++) {
-    console.log(offElems[i].style.display);
     offElems[i].style.display = '';
   };
   for (var i = 0; i < onElems.length; i++) {
